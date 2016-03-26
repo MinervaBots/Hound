d1ca72a1a1ed73e0440d4df45db7c6b7d1607921
Amanda <amandacnoliveira@poli.ufrj.br>
2016-03-26 10:45:15 -0300

    Atualização de pinos
    falta atualizar motores e conferir o botão 3 (chave 1) e os switches

--- 2016.1/Trekking/trekkingpins.h
+++ 2016.1/Trekking/trekkingpins.h
@@ -1,9 +1,9 @@
 /*
 	Buttons and switches
 */
-const byte INIT_BUTTON_PIN = A4;
-const byte EMERGENCY_BUTTON_PIN  = A5;
-const byte OPERATION_MODE_SWITCH_PIN = A3;
+const byte INIT_BUTTON_PIN = 3;
+const byte EMERGENCY_BUTTON_PIN  = 2;
+const byte OPERATION_MODE_SWITCH_PIN = A3; \\ Não está atualizado!
 
 const bool MANUAL_MODE = 0;
 const bool AUTO_MODE = 1;
@@ -22,21 +22,21 @@ const byte L_MOTOR_2_PIN = 11;
 /*
 	Sonars
 */
-const byte LEFT_SONAR_RX_PIN = 9;
-const byte LEFT_SONAR_TX_PIN = A0;
+const byte LEFT_SONAR_RX_PIN = 17;
+const byte LEFT_SONAR_TX_PIN = A11;
 const byte CENTER_SONAR_RX_PIN = 0;
-const byte CENTER_SONAR_TX_PIN = A1;
+const byte CENTER_SONAR_TX_PIN = A10;
 const byte RIGHT_SONAR_RX_PIN = 0;
-const byte RIGHT_SONAR_TX_PIN = A2;
+const byte RIGHT_SONAR_TX_PIN = A7;
 
 
 /*
 	Sirene
 */
-const byte SIRENE_PIN = 12;
+// const byte SIRENE_PIN = 12;
 
 /*
 	Alert led
 */
 
-const byte ALERT_LED = A6;
+const byte ALERT_LED = 13;
