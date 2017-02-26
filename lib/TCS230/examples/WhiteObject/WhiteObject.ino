90c295890012d2d11932a384389533632e5e3bb5
dilss <edilson.fernandes@poli.ufrj.br>
2017-02-26 15:12:17 -0300

    Correções de inconsistencia nos código exemplo da lib

--- lib/TCS230/examples/WhiteObject/WhiteObject.ino
+++ lib/TCS230/examples/WhiteObject/WhiteObject.ino
@@ -14,19 +14,19 @@
 #define OUTPUT_ENEABLE 7 //define output eneable pin of the sensor
 
 
-TCS230 new_sensor(8,9,12,11,10); //inicialize a new sensor with the given pins
+TCS230 new_sensor(8,9,12,11,10,50); //inicialize a new sensor with the given pins
 
 
 //the setup function runs once when you press reset or power the board
 void setup() {
-	new_sensor.calibrate("branco"); //calibrate sensor for the white color
 	pinMode(OUTPUT_ENEABLE, INPUT);
 	digitalWrite(OUTPUT_ENEABLE, HIGH);
+  Serial.begin(9600);
 }
 
 //the loop function runs over and over again forever
 void loop() {
-	new_sensor.color();
-	new_sensor.isTheColor();
+  if(new_sensor.isWhite()) Serial.println("The surface it is indeed white!");
+  else Serial.println("Not white Bro!");
 	delay(500);  
 }
